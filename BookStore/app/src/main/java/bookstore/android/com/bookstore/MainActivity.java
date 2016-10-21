3d39011a92727731c13c8763ca7e8f36eefe5cac
PHOTON\srinisha_s <srinishasridhar@gmail.com>
2016-10-21 20:41:17 +0530

    Test  commit

--- BookStore/app/src/main/java/bookstore/android/com/bookstore/MainActivity.java
+++ BookStore/app/src/main/java/bookstore/android/com/bookstore/MainActivity.java
@@ -17,6 +17,7 @@ private TextView forgotPassword;
         forgotPassword=(TextView)findViewById(R.id.forgot_password);
         forgotPassword.setPaintFlags(forgotPassword.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         forgotPassword.setText(R.string.forgot_password);
+        
 
     }
 
