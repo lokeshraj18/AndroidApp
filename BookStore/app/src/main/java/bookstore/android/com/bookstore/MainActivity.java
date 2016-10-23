3f048303bce1a40b98ca8e6d32691e8f48aa6e93
Lenovo-PC\Srinisha <srinishasridhar@gmail.com>
2016-10-23 20:42:33 +0530

    Forgot Password Changes

--- BookStore/app/src/main/java/bookstore/android/com/bookstore/MainActivity.java
+++ BookStore/app/src/main/java/bookstore/android/com/bookstore/MainActivity.java
@@ -1,5 +1,6 @@
 package bookstore.android.com.bookstore;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,18 +17,28 @@ private TextView forgotPassword;
     private EditText mobileEditText;
     private EditText passEditText;
 
+public void forgotPassword(){
+    forgotPassword=(TextView)findViewById(R.id.forgot_password);
+    forgotPassword.setPaintFlags(forgotPassword.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    forgotPassword.setText(R.string.forgot_password);
+    forgotPassword.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i=new Intent(MainActivity.this,ForgotPassword.class);
+            startActivity(i);
+        }
+    });
 
+
+
+
+}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        forgotPassword=(TextView)findViewById(R.id.forgot_password);
-        forgotPassword.setPaintFlags(forgotPassword.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        forgotPassword.setText(R.string.forgot_password);
-
-
-        
+        forgotPassword();
 
     }
 
