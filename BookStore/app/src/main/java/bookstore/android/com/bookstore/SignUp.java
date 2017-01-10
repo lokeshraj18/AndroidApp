5f58668824e1557198099d3f7d534e2e154d3798
Lenovo-PC\Srinisha <srinishasridhar@gmail.com>
2017-01-10 11:00:31 +0530

    drop down and details screen changes

--- BookStore/app/src/main/java/bookstore/android/com/bookstore/SignUp.java
+++ BookStore/app/src/main/java/bookstore/android/com/bookstore/SignUp.java
@@ -1,37 +1,43 @@
 package bookstore.android.com.bookstore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class SignUp extends AppCompatActivity {
+
+    private Spinner mSpinner;
+
+    ArrayAdapter<String> mAdapter;
+
+    private Button cancel;
+    private Button submit;
+    public String[] locations;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        locations=getResources().getStringArray(R.array.location);
+        mSpinner=(Spinner)findViewById(R.id.spin);
+        mAdapter=new ArrayAdapter<String>(this,R.layout.activity_dropdownview,R.id.drop,locations);
+        mSpinner.setAdapter(mAdapter);
+        cancel=(Button)findViewById(R.id.signup_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getBaseContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
